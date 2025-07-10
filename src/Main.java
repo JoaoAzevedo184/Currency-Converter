2645a5a0f795a64a3be555a201b137c87c74425c
JoaoAzevedo184 <ccjconrad50@gmail.com>
2025-07-09 22:01:40 -0300

    interação com usuário p1

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,42 @@
+import api.ExchangeApiClient;
+import util.ConsoleView;
+import util.CurrencyConversionHistory;
+import util.FileGenerater;
+
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        try {
+            Scanner scanner = new Scanner(System.in);
+            FileGenerater fileGenerater = new FileGenerater();
+            ExchangeApiClient api = new ExchangeApiClient();
+            CurrencyConversionHistory history = new CurrencyConversionHistory();
+            int escolha;
+
+            while (true) {
+                ConsoleView.mostrarMenu();
+                escolha = scanner.nextInt();
+
+                switch (escolha) {
+                    case 1 -> ConsoleView.converterDinheiro();
+                    case 2 -> ConsoleView.mostrarHistorico(history);
+                    case 3 -> {
+                        System.out.println("\n▷ Salvando histórico completo...");
+                        fileGenerater.salvarArquivo(history.getAll());
+                        ConsoleView.fimPrograma();
+                    }
+                    default -> {
+                        System.out.println("\n❌ Entrada inválida. Digite um número de 1 a 3.";
+                    }
+                }
+                ConsoleView.solicitarValor();
+            }
+        } catch (RuntimeException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
